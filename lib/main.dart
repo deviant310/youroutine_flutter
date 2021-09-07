f55072b12e92774c1da58d07e4d7ddd71e6cca1f
Anton Lebedev <dunloop8@gmail.com>
2021-09-07 18:23:59 +0300

    creating login screen - adding fontawesome icons, init phone editing controller with phone mask formatter

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'styles/palette.dart';
 import 'styles/decorations.dart';
@@ -34,6 +35,31 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
+  final countryEditingController = TextEditingController(text: 'Россия');
+  final phoneEditingController = TextEditingController();
+  final phoneMaskFormatter = MaskTextInputFormatter(
+    mask: '+# (###) ###-##-##',
+    filter: {"#": RegExp(r'[0-9]')},
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    countryEditingController.addListener(_printLatestValue);
+    phoneEditingController.value = phoneMaskFormatter.formatEditUpdate(TextEditingValue(text: ''), TextEditingValue(text: '7'));
+  }
+
+  @override
+  void dispose() {
+    countryEditingController.dispose();
+    phoneEditingController.dispose();
+    super.dispose();
+  }
+
+  void _printLatestValue() {
+    print(countryEditingController.text);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,8 +75,8 @@ class _LoginScreenState extends State<LoginScreen> {
               child: Column(
                 children: <Widget>[
                   Container(
-                    width: 120,
-                    height: 120,
+                    width: 110,
+                    height: 110,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(60),
                         color: Palette.PrimaryBlueToDark),
@@ -69,7 +95,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     'Вход в YouRoutine',
                     style: TextStyle(
                       color: Palette.TextBlueToLight,
-                      fontSize: 26,
+                      fontSize: 25,
                       fontWeight: FontWeight.w500,
                     ),
                   ),
@@ -78,45 +104,79 @@ class _LoginScreenState extends State<LoginScreen> {
                     'Пожалуйста, укажите свою страну и\u{00A0}введите свой номер телефона.',
                     textAlign: TextAlign.center,
                     style: TextStyle(
-                      height: 1.3,
+                      height: 1.2,
                       color: Palette.TextBlueToLight,
-                      fontSize: 15,
+                      fontSize: 14,
                     ),
                   ),
+                  SizedBox(height: 35),
                   Form(
                     key: _formKey,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        TextFormField(
-                          cursorColor: Palette.TextBlueToLight,
-                          style: TextStyle(color: Palette.TextBlueToLight),
-                          decoration: decorations.input,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
+                        Row(
+                          children: [
+                            FaIcon(
+                              FontAwesomeIcons.solidGlobe,
+                              color: Palette.TextBlueToLight.shade300,
+                              size: 20,
+                            ),
+                            SizedBox(width: 20),
+                            Expanded(
+                              child: TextFormField(
+                                controller: countryEditingController,
+                                cursorColor: Palette.TextBlueToLight,
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Palette.TextBlueToLight,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                decoration: decorations.input,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter some text';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                          ],
                         ),
-                        TextFormField(
-                          cursorColor: Palette.TextBlueToLight,
-                          style: TextStyle(color: Palette.TextBlueToLight),
-                          decoration: decorations.input,
-                          keyboardType: TextInputType.number,
-                          inputFormatters: [
-                            MaskTextInputFormatter(
-                              mask: '+# (###) ###-##-##',
-                              filter: {"#": RegExp(r'[0-9]')},
+                        SizedBox(height: 20),
+                        Row(
+                          children: [
+                            FaIcon(
+                              FontAwesomeIcons.solidPhoneAlt,
+                              color: Palette.TextBlueToLight.shade300,
+                              size: 20,
+                            ),
+                            SizedBox(width: 20),
+                            Expanded(
+                              child: TextFormField(
+                                controller: phoneEditingController,
+                                cursorColor: Palette.TextBlueToLight,
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Palette.TextBlueToLight,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                decoration: decorations.input,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  phoneMaskFormatter
+                                ],
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter some text';
+                                  }
+                                  return null;
+                                },
+                              ),
                             )
                           ],
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
                         ),
+                        SizedBox(height: 30),
                         Container(
                           padding: const EdgeInsets.symmetric(vertical: 16.0),
                           child: ElevatedButton(
